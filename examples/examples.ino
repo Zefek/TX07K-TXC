be27c1180c5ab73dfc2e55f563629c00b5b5bdaa
Zefek <32429493+Zefek@users.noreply.github.com>
2026-04-20 11:22:29 +0200

    Update examples.ino

--- examples/examples.ino
+++ examples/examples.ino
@@ -6,9 +6,8 @@ TX07KTXC outsideTemperatureSensor(2, 3, OutsideTemperatureChanged);
 
 void OutsideTemperatureChanged(double temperature, uint8_t channel, uint8_t sensorId, uint8_t* rawData, bool transmitedByButton)
 {  
-  Serial.write(rawData);
-  Serial.write(13);
-  Serial.write(10);
+  Serial.write(rawData, 5); 
+  Serial.write("\r\n");
 }
 
 void setup()
