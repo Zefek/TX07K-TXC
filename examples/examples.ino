31d4008090fb70e8170d5f65cd840d36f1b7ce13
Zefek <32429493+Zefek@users.noreply.github.com>
2026-04-20 13:40:46 +0200

    Example (#10)
    * Add temperature sensor handling in example.ino
    
    * Add GitHub Actions workflow for Arduino deployment
    
    * Add example directory to Arduino project compilation
    
    * Rename example.ino to example.ino
    
    * Rename example.ino to examples.ino
    
    * Update deployment.yml
    
    * Update build.yml
    
    * Update build.yml
    
    * Update examples.ino
    
    * Update examples.ino

---
+++ examples/examples.ino
@@ -0,0 +1,22 @@
+#include <TX07K-TXC.h>
+
+void OutsideTemperatureChanged(double temperature, uint8_t channel, uint8_t sensorId, uint8_t* rawData, bool transmitedByButton);
+
+TX07KTXC outsideTemperatureSensor(2, 3, OutsideTemperatureChanged);
+
+void OutsideTemperatureChanged(double temperature, uint8_t channel, uint8_t sensorId, uint8_t* rawData, bool transmitedByButton)
+{  
+  Serial.write(rawData, 5); 
+  Serial.write("\r\n");
+}
+
+void setup()
+{
+  Serial.begin(9600);
+  outsideTemperatureSensor.Init();
+}
+
+void loop()
+{
+  outsideTemperatureSensor.CheckTemperature();
+}
